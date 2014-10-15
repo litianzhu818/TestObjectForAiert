19af9edd776ae4434b88c8d1fca54fa02c8308b9
李天柱 <PeterLee@PeterdeMacBook-Pro.local>
2014-10-15 14:10:54 +0800

    增加了一个视屏播放的viewcontroller类到这个工程

---
+++ AiertApp/ViewControllers/VideoPlayerViewControllers/VideoPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayerViewController.h
+//  AiertApp
+//
+//  Created by  李天柱 on 14-10-15.
+//  Copyright (c) 2014年 爱尔特电子有限公司. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface VideoPlayerViewController : BaseViewController
+
+@end
