01cf8529dc4cd62ad58ebb4176b800359611ef9b
Peter Lee <peter.lee@afusion.com>
2014-10-30 11:27:00 +0800

    增加了IOTC的库文件

--- AiertApp/UtilityManagers/CoreDataStorage/include/CoreDataStorage.h
+++ AiertApp/UtilityManagers/CoreDataStorage/include/CoreDataStorage.h
@@ -1,41 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
-/**
- * This class provides an optional base class that may be used to implement
- * a CoreDataStorage class for an xmpp extension (or perhaps any core data storage class).
- * 
- * It operates on its own dispatch queue which allows it to easily provide storage for multiple extension instance.
- * More importantly, it smartly buffers its save operations to maximize performance!
- * 
- * It does this using two techniques:
- * 
- * First, it monitors the number of pending requests.
- * When a operation is requested of the class, it increments an atomic variable, and schedules the request.
- * After the request has been processed, it decrements the atomic variable.
- * At this point it knows if there are other pending requests,
- * and it uses the information to decide if it should save now,
- * or postpone the save operation until the pending requests have been executed.
- * 
- * Second, it monitors the number of unsaved changes.
- * Since NSManagedObjectContext retains any changed objects until they are saved to disk
- * it is an important memory management concern to keep the number of changed objects within a healthy range.
- * This class uses a configurable saveThreshold to save at appropriate times.
- * 
- * This class also offers several useful features such as
- * preventing multiple instances from using the same database file (conflict)
- * and caching of xmppStream.myJID to improve performance.
- * 
- * For more information on how to extend this class,
- * please see the XMPPCoreDataStorageProtected.h header file.
- * 
- * The framework comes with several classes that extend this base class such as:
- * - XMPPRosterCoreDataStorage       (Extensions/Roster)
- * - XMPPCapabilitiesCoreDataStorage (Extensions/XEP-0115)
- * - XMPPvCardCoreDataStorage        (Extensions/XEP-0054)
- * 
- * Feel free to skim over these as reference implementations.
-**/
 
 @interface CoreDataStorage : NSObject {
 @private
@@ -69,7 +34,7 @@
 
 /**
  * Initializes a core data storage instance, backed by SQLite, with the given database store filename.
- * It is recommended your database filname use the "sqlite" file extension (e.g. "XMPPRoster.sqlite").
+ * It is recommended your database filname use the "sqlite" file extension (e.g. "AiertDeviceInfo.sqlite").
  * If you pass nil, a default database filename is automatically used.
  * This default is derived from the classname,
  * meaning subclasses will get a default database filename derived from the subclass classname.
