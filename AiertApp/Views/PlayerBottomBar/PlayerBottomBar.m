2108894976e9c8235ecf1ad63a330bf69a4cc8e5
Peter Lee <peter.lee@afusion.com>
2014-11-10 14:53:39 +0800

    增加了播放器的topBar 和bottom bar

---
+++ AiertApp/Views/PlayerBottomBar/PlayerBottomBar.m
@@ -0,0 +1,61 @@
+//
+//  PlayerBottomBar.m
+//  AiertApp
+//
+//  Created by Peter Lee on 14/11/10.
+//  Copyright (c) 2014年 爱尔特电子有限公司. All rights reserved.
+//
+
+#import "PlayerBottomBar.h"
+
+@implementation PlayerBottomBar
+
++ (instancetype)instanceFromNib
+{
+    return [[[NSBundle mainBundle] loadNibNamed:@"PlayerBottomBar" owner:self options:nil] lastObject];
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+/*
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    //your code here
+    
+    [super touchesBegan:touches withEvent:event];
+}
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    //your code here
+    
+    // check touch up inside
+    if ([self superview]) {
+        UITouch *touch = [touches anyObject];
+        CGPoint point = [touch locationInView:[self superview]];
+        //TODO:这里可以将触摸范围扩大，便于操作，例如：
+         CGRect validTouchArea = CGRectMake((self.frame.origin.x - 10),
+         (self.frame.origin.y - 10),
+         (self.frame.size.width + 10),
+         (self.frame.size.height + 10));
+        if (CGRectContainsPoint(validTouchArea, point)) {
+            //your code here
+        }
+    }
+    
+    [super touchesEnded:touches withEvent:event];
+}
+*/
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ AiertApp/Views/PlayerTopBar/PlayerTopBar.h
@@ -0,0 +1,15 @@
+//
+//  PlayerTopBar.h
+//  AiertApp
+//
+//  Created by Peter Lee on 14/11/10.
+//  Copyright (c) 2014年 爱尔特电子有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerTopBar : UIView
+
++ (instancetype)instanceFromNib;
+
+@end
